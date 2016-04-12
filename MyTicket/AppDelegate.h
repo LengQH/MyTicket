be4b63d989e48c0eb851aa68f2ff545a13eaf606
LengBoby <Leng@lengqiuhuidemacbook-pro.local>
2016-04-12 23:18:22 +0800

    Initial Commit

---
+++ MyTicket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTicket
+//
+//  Created by 冷求慧 on 16/4/12.
+//  Copyright © 2016年 leng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTicket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTicket
+//
+//  Created by 冷求慧 on 16/4/12.
+//  Copyright © 2016年 leng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
